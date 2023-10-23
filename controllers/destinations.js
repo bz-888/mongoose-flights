a8250c2d6047cfe23b96c839a5ce92cdbba83e18
bz-888 <benjamin.zheng95@gmail.com>
2023-10-23 18:55:18 -0500

    completed part 2

--- controllers/destinations.js
+++ controllers/destinations.js
@@ -4,7 +4,18 @@ module.exports = {
     create
 }
 
-function create(req, res) {
-    console.log(req.params.id, "<-- req.params.id");
-    console.log(req.body, "<-- req.body")
+async function create(req, res) {
+    try {
+        const flightDoc = await flightModel.findById(req.params.id);
+        console.log(req.params.id, "<-- req.params.id");
+        console.log(flightDoc, "<-- flightDoc");
+        console.log(req.body, "<-- req,body");
+        flightDoc.destination.push(req.body);
+        console.log(flightDoc);
+        await flightDoc.save();
+        res.redirect(`/flights/${req.params.id}`);
+    } catch(err) {
+        console.log(err, "<-- ERROR");
+        res.send(err);
+    }
 }
--- controllers/flights.js
+++ controllers/flights.js
@@ -10,9 +10,9 @@ module.exports = {
 async function index(req, res) {
     try {
         const flightDocuments = await flightModel.find({});
-        console.log("all flights from flights/index below")
-        console.log("flightDocuments", flightDocuments)
-        console.log("all flights from flights/index above")
+        console.log("all flights from flights/index below");
+        console.log("flightDocuments", flightDocuments);
+        console.log("all flights from flights/index above");
         res.render("flights/index", {flightDocs: flightDocuments});
     } catch(err) {
         res.send(err);
@@ -39,5 +39,5 @@ async function show(req, res) {
     // console.log(req.params.id);
     const flightDoc = await flightModel.findById(req.params.id);
     console.log(flightDoc, "<-- show function flight");
-    res.render("flights/show", { flight: flightDoc});
+    res.render("flights/show", { flightShow: flightDoc});
 }
